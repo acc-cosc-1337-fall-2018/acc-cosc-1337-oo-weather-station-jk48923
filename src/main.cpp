568a538b108cb8b25f929c00cd726f5b4b4ee318
Jeremiah <jeremiah.kleidon@g.austincc.com>
2018-09-25 18:55:34 -0500

    change 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
 #include "clock.h"
+#include<thread>
 
 int main() 
 {
 	Clock clock;
+	
 
-	clock.display_time();
+	int i{ 0 };
+	while (i != 100)
+	{
+		std:this_thread::sleep_for(std::chrono::seconds(1))
+		clock.display_time();
+		std::_Count_pr < std::endl;
+		i++;
+	}
+
 	
 	return 0;
 }
