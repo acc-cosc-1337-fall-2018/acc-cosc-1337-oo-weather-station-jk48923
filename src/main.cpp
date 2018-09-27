f541d536219710b067856d5f162482af2b336b6c
Jeremiah <jeremiah.kleidon@g.austincc.com>
2018-09-27 18:28:58 -0500

    class edits

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,23 @@
 #include "clock.h"
+#include<iostream>
 #include<thread>
+#include "temperature_observation.h"
 
 int main() 
 {
+
+	TemperatureObservation observ(90, 15000);
+
+
 	Clock clock;
 	
 
 	int i{ 0 };
 	while (i != 100)
 	{
-		std:this_thread::sleep_for(std::chrono::seconds(1))
+		std::this_thread::sleep_for(std::chrono::seconds(1));
 		clock.display_time();
-		std::_Count_pr < std::endl;
+		std::cout << std::endl;
 		i++;
 	}
 
--- src/model/temperature_observation.h
+++ src/model/temperature_observation.h
@@ -1,11 +1,12 @@
 #ifndef TEMPERATURE_OBSERVATION_H
 #define TEMPERATURE_OBSERVATION_H
 
-class TEMPERATUREOBSERVATION
+class TemperatureObservation
 {
 
 public:
 	TemperatureObservation(int t, long long s) : temperature(t), seconds(s) {}
+	int get_temperature() const;
 
 
 private:
